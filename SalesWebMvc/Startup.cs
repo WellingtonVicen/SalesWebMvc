6ecfa60f3bf120e6883564c120d1ea77561272f0
WellingtonVicen <wellvicencio@gmail.com>
2020-08-07 19:33:16 -0300

    Localização e formatação de números e datas

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+
 namespace SalesWebMvc
 {
     public class Startup
@@ -48,7 +49,17 @@ namespace SalesWebMvc
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
 
-           
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
